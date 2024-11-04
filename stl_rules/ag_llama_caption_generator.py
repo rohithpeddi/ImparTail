7f6e2d4b722309b5f0e2c8ce1d27ca7aa0583df2
rxp190007 <rxp190007@utdallas.edu>
2024-11-04 08:04:17 -0600

    [ImageCaptions] Using Llama-3.2B

--- stl_rules/ag_llama_caption_generator.py
+++ stl_rules/ag_llama_caption_generator.py
@@ -21,6 +21,8 @@ class AgLLamaCaptionGenerator:
         self.processor = AutoProcessor.from_pretrained(self.model_id)
 
         self.video_frames_dir = "/data/rohith/ag/frames_annotated/"
+        self.captions_dir = "/data/rohith/ag/captions/llame_3_2_11B/"
+        os.makedirs(self.captions_dir, exist_ok=True)
 
     def generate_video_frame_captions(self, video_id):
 
@@ -33,7 +35,16 @@ class AgLLamaCaptionGenerator:
             messages = [
                 {"role": "user", "content": [
                     {"type": "image"},
-                    {"type": "text", "text": " "}
+                    {"type": "text",
+                     "text": "You are an expert in describing objects and relationships in images."
+                             "Can you describe the image and make sure to focus only on the objects "
+                             "from the following categories: [person, bag, bed, blanket, book, box, broom, chair, closet/cabinet, clothes, "
+                             "cup/glass/bottle, dish, door, doorknob, doorway, floor, food, groceries, laptop, light, medicine, mirror, "
+                             "paper/notebook, phone/camera, picture, pillow, refrigerator, sandwich, shelf, shoe, sofa/couch, "
+                             "table, television, towel, vacuum, window] and for each pair of the observed "
+                             "objects describe relationships between them from the following categories : [looking at, not looking at, unsure, above, beneath, in front of, behind, on the side of, "
+                             "in, carrying, covered by, drinking from, eating, have it on the back, "
+                             "holding, leaning on, lyingon, not contacting, other relationship, sitting on, standing on, touching, twisting, wearing, wiping, writingon]"}
                 ]}
             ]
             input_text = self.processor.apply_chat_template(messages, add_generation_prompt=True)
@@ -44,10 +55,11 @@ class AgLLamaCaptionGenerator:
                 return_tensors="pt"
             ).to(self.model.device)
 
-            output = self.model.generate(**inputs, max_new_tokens=128)
+            output = self.model.generate(**inputs, max_new_tokens=384)
             video_image_caption_json[video_frame[:-4]] = self.processor.decode(output[0])
 
-        with open(f"/data/rohith/ag/captions/llame_3_2_11B/{video_id[:-4]}.json", "w") as f:
+        video_caption_path = os.path.join(self.captions_dir, f"{video_id[:-4]}.json")
+        with open(video_caption_path, "w") as f:
             json.dump(video_image_caption_json, f)
 
     def generate_video_captions(self):
